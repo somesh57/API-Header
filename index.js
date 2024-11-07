72f7b077f8a2f0fccbcca432ba6fbafd595745bc
somesh57 <151932387+somesh57@users.noreply.github.com>
2024-11-07 18:41:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-// Routes ko import karna
+
 const headersRoute = require("./Routes/Headers");
 
-// Use the routes
+
 app.use("/", headersRoute); // Ye sahi use hai
 
-// Server ko start karna
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });

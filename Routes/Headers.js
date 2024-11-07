9f291f21617b0c7264872f575303e7cdf2054648
somesh57 <151932387+somesh57@users.noreply.github.com>
2024-11-07 18:41:04 +0530

    Update Headers.js

--- Routes/Headers.js
+++ Routes/Headers.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-// Get all headers from request
+
 router.get("/get-headers", (req, res) => {
   const headers = req.headers;
   res.json(headers);
